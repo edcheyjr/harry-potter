6478c939a07d9411d6e3e789c9fdaab6f01ff55f
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-14 01:38:37 +0300

    style changes

--- src/components/Character.tsx
+++ src/components/Character.tsx
@@ -2,7 +2,7 @@
 
 import React, { MouseEvent, useState } from 'react'
 import Image from 'next/image'
-import DefaultImage from 'public/Harry_Potter_owl.jpg'
+import DefaultImage from '/public/Harry_Potter_owl.jpg'
 import { Houses } from 'types.d'
 import { houseColors } from 'utils/colors'
 
@@ -27,33 +27,19 @@ const CharacterCard = ({ DOB, house, imageSrc, id, name, YOB }: Props) => {
       id={id}
       onClick={moveToCharacterPage}
       aria-label={name || 'unknown'}
-      className={`group justify-self-stretch flex flex-col justify-end rounded h-96 transform transtion ease-in-out duration-300 cursor-pointer group-hover:${
-        house ? `border-[${houseColors[house].primary}]` : 'border-orange-500 '
+      className={`group justify-self-stretch flex flex-col justify-end rounded h-96 transform transtion-all ease-in-out duration-300 cursor-pointer border border-transparent ${
+        house
+          ? `hover:border-[${houseColors[house].primary}]`
+          : 'hover:border-orange-500 '
       }`}
       style={{
         backgroundImage: `url(${imageSrc || DefaultImage.src})`,
       }}
     >
-      <div className='w-full flex bg-bg-dark/10 backdrop-blur-md space-x-2 px-5 py-2'>
-        {/* crest */}
-        {house && (
-          <div className='w-7 h-auto'>
-            <Image
-              src={require(`/public/crests/${house}.png`)}
-              className='h-7 w-7'
-              width={20}
-              height={20}
-              alt={house || 'no house'}
-            />
-          </div>
-        )}
+      <div className='w-full flex bg-bg-dark/10 backdrop-blur-md space-x-2 px-5 py-2 justify-between items-center'>
         <div className='flex flex-col space-y-2'>
           <h6
-            className={`text-lg lg:text-xl xl:text-xl font-bold text-slate-50 capitalize text-ellipsis group-hover:text-transparent group-hover:bg-clip-text group-hover:bg-gradient-to-br ${
-              house
-                ? `group-hover:from-primary-${houseColors[house].primary} group-hover:via-[${houseColors[house].primary}] group-hover:to-[${houseColors[house].secondary}]`
-                : 'from-red-500 via-orange-500 to-amber-500'
-            }]`}
+            className={`text-lg lg:text-xl xl:text-xl font-bold text-slate-50 capitalize text-ellipsis group-hover:text-transparent group-hover:bg-clip-text group-hover:bg-gradient-to-br ${'group-hover:from-red-500 group-hover:to-amber-500 group-hover:via-orange-500 '}]`}
           >
             {name || 'unknown'}
           </h6>
@@ -61,6 +47,16 @@ const CharacterCard = ({ DOB, house, imageSrc, id, name, YOB }: Props) => {
             {DOB || YOB || 'No DoB'}
           </p>
         </div>
+        {/* crest */}
+        {house && (
+          <div className='w-auto h-auto my-auto  '>
+            <Image
+              src={require(`/public/crests/${house}.png`)}
+              className='h-auto w-10'
+              alt={house || 'no house'}
+            />
+          </div>
+        )}
       </div>
     </article>
   )
