09429ee459a2545fef8abe9bc9327663fa666f58
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-13 16:00:21 +0300

    bg color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,11 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors: {
+        bg: {
+          dark: 'rgb(14, 22, 34)',
+        },
+      },
     },
   },
   plugins: [],
