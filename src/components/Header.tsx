91b083ede93c845d183352c5e6e034e0e875125f
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-13 15:58:37 +0300

    header component update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,35 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import HarryPotterText from 'harrypotter.svg'
 import Image from 'next/image'
 import localFont from 'next/font/local'
+import NavBar from './NavBar'
+import HarryPotterWorkMark from './icons/HarryPotterWordmark'
 
 const harry = localFont({ src: '../../public/fonts/local/HarryP.ttf' })
+// const ysabeau =
+//
 
 type Props = {}
 
 const Header = (props: Props) => {
+  // const [color, setColor] = useState<string>(houseColors.Gryffindor.primary)
   return (
     <header
-      className='h-2/3 
+      className='
+      min-h-screen
       w-full 
-      max-w-xl
+      mx-auto
       bg-gradient-to-b
-      from-black/30
-      via-transparent
-      to-black/30
+      from-bg-dark
+      via-bg-dark/30
+      to-bg-dark/95
       '
-      style={{ background: `url("/public/harry-potter-books.jpg"})` }}
     >
-      {/* Nav */}
+      <div className='container mx-auto max-w-7xl px-10 flex flex-col justify-center pt-8'>
+        {/* Nav */}
+        <section className='space-y-2 flex flex-col items-center justify-bottom'>
+          {/* Heading Text */}
+          <h1
+            className={`${harry.className} text-4xl md:text-7xl xl:text-9xl capitalize text-white`}
+          >
+            <HarryPotterWorkMark
+              title='harry potter'
+              titleId='harry potter'
+              className='fill-current w-[32rem]'
+            />
+          </h1>
 
-      <section>
-        {/* Heading Text */}
-        <h1 className={`${harry.className} text-9xl capitalize `}>
-          Harry Potter
-        </h1>
-
-        {/* Description */}
-      </section>
+          {/* Description */}
+          <p className='text-lg lg:text-xl xl:text-2xl font-medium text-slate-300'>
+            Characters, Know them and the world of wizardry
+          </p>
+        </section>
+      </div>
     </header>
   )
 }
