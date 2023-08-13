55ac699ba888a9b3d5d48b6f7269ad129c5f292c
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-13 23:52:29 +0300

    page updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,20 @@
+import CharactersSection from 'components/CharactersSection'
 import bg from '/public/harry-potter-books.jpg'
 import Hero from 'components/Hero'
 
 export default function Home() {
   return (
     <main
-      className='min-h-screen bg-fixed'
+      className='h-full w-full flex flex-col bg-fixed'
       style={{ backgroundImage: `url(${bg.src})` }}
     >
       {/* hero */}
-      <section>
+      <section className=' w-full mx-auto bg-gradient-to-b from-bg-dark from-5% via-bg-dark/80 via-60% to-bg-dark/40 '>
         <Hero />
       </section>
       {/* Scroll up gris sytem for the cards */}
-      <section className='bg-bg-dark h-2/3'>
-        <div className='container mx-auto max-w-7xl px-10 '>sad</div>
+      <section className=' w-full bg-gradient-to-t from-bg-dark from-45% via-bg-dark/[80%] to-bg-dark/40 space-y-5'>
+        <CharactersSection />
       </section>
     </main>
   )
