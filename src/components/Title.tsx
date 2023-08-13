8ea3252a61988db30ed95917b2a0235a77a0187f
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-14 01:41:06 +0300

    Title compoent

---
+++ src/components/Title.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { ColorPair } from '../../types'
+import localFont from 'next/font/local'
+
+const harry = localFont({ src: '../../public/fonts/local/HarryP.ttf' })
+
+type Props = {
+  title: string
+  desc?: string
+  colors?: ColorPair
+}
+
+const Title = ({ title, desc }: Props) => {
+  return (
+    <div className='flex w-full items-center space-x-8'>
+      <h3
+        className={`${harry.className} uppercase font-bold tracking-[5px] text-lg lg:text-xl xl:text-2xl bg-clip-text bg-gradient-to-br from-red-500 from-20% via-orange-500 to-amber-500 text-transparent `}
+      >
+        {title}
+      </h3>
+      <p className='text-slate-500 text-base lg:text-lg first-letter:uppercase'>
+        {desc}
+      </p>
+    </div>
+  )
+}
+
+export default Title
