d2c4bd02daa2d913c32b2f1b623dfca0aaed878d
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-13 16:00:05 +0300

    search button

--- src/components/SearchButton.tsx
+++ src/components/SearchButton.tsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import SearchIcon from './icons/SearchIcon'
 
 type Props = {}
 
 const SearchButton = (props: Props) => {
-  return <div>SearchButton</div>
+  return (
+    <button className='px-3.5 rounded bg-white/[8%] flex space-x-2.5 text-gray-50/80 items-center border border-gray-50/20 hover:bg-white/20 transform transition-all ease-in-out duration-300'>
+      <SearchIcon
+        className='h-5 w-5 lg:h-6 lg:w-6 fill-current'
+        fill='currentIcon'
+      />
+      <span className='text-sm font-regular uppercase'>search...</span>
+      <span
+        title='shortcut'
+        className='ml-4 py-1 rounded px-2 text-center border-r border-l border-t border-b-2 bg-bg-dark border-gray-50/[25%] text-sm lg:text-base font-semibold tracking-tight'
+      >
+        Ctrl K
+      </span>
+    </button>
+  )
 }
 
 export default SearchButton
