0218a0d1e4c29837000a3b4ee56f267931e98829
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-13 16:56:17 +0300

    fixed navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Logo } from './Logo'
 import SearchButton from './SearchButton'
 
@@ -6,7 +6,9 @@ type Props = {}
 
 const NavBar = (props: Props) => {
   return (
-    <nav className='border-b border-orange-500/80'>
+    <nav
+      className={`border-b border-orange-500/80 fixed -top-0 left-0 right-0 transform transition-all ease-in-out duration-500 bg-bg-dark/25 backdrop-blur-2xl`}
+    >
       <div className='flex justify-between container mx-auto max-w-7xl px-10 py-4'>
         <Logo />
         <SearchButton />
