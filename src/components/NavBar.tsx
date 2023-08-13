c7e60dc33762c2e65d5bffc2cf80d4312d9052d1
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-13 23:50:24 +0300

    z index

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,7 +7,7 @@ type Props = {}
 const NavBar = (props: Props) => {
   return (
     <nav
-      className={`border-b border-orange-500/80 fixed -top-0 left-0 right-0 transform transition-all ease-in-out duration-500 bg-bg-dark/25 backdrop-blur-2xl`}
+      className={`z-10 border-b border-orange-500/80 fixed -top-0 left-0 right-0 transform transition-all ease-in-out duration-500 bg-bg-dark/25 backdrop-blur-2xl`}
     >
       <div className='flex justify-between container mx-auto max-w-7xl px-10 py-4'>
         <Logo />
