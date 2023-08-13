53fa9ae9ae9b00e54899b0be842a390d8b6b2014
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-13 23:49:10 +0300

    character type

--- types.d.ts
+++ types.d.ts
@@ -5,9 +5,38 @@ export enum Houses {
   SLYTHERIN = 'Slytherin',
 }
 
-interface ColorPair {
+export interface ColorPair {
   primary: string
   secondary: string
 }
 
 export type HouseColors = Record<Houses, ColorPair>
+
+export interface Character {
+  id: string
+  name: string
+  alternate_names: String[]
+  species?: string
+  gender?: string
+  house?: string
+  dateOfBirth?: string
+  yearOfBirth?: number
+  wizard: boolean
+  ancestry?: string
+  eyeColour?: string
+  hairColour?: string
+  wand?: WandType
+  patronus: string
+  hogwartsStudent: boolean
+  hogwartsStaff: boolean
+  actor: string
+  alternate_actors: String[]
+  alive: boolean
+  image?: string
+}
+
+interface WandType {
+  wood: string
+  core: string
+  length: number
+}
