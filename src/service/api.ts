b7eadd947547caa762b6f33addace09613b8f0b8
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-14 15:30:40 +0300

    service api

---
+++ src/service/api.ts
@@ -0,0 +1,62 @@
+import { Character, Houses, Spell } from 'types.d'
+import { APIUrl } from 'utils/constant'
+
+console.log('apiURl', APIUrl)
+
+type FilterTypes = {
+  filter?: 'students' | 'staff' | 'house'
+  houseID?: Houses
+}
+
+async function fetchAllCharacters({
+  filter,
+  houseID,
+}: FilterTypes): Promise<Character[]> {
+  const BaseUrl = `${APIUrl}/characters`
+  let Url = ''
+  if (filter) {
+    if (filter == 'house') {
+      Url = `${BaseUrl}/${filter}/${houseID}`
+    }
+    Url = `${BaseUrl}/${filter}`
+  }
+  try {
+    const response = await fetch(Url)
+    const data = await response.json()
+    if (response.status != 200) {
+      throw data
+    }
+    return data as Character[]
+  } catch (error) {
+    console.log(error)
+  }
+  return []
+}
+
+async function fetchACharacter(id: string): Promise<Character[]> {
+  try {
+    const response = await fetch(`${APIUrl}/character/${id}`)
+    const data = await response.json()
+    if (response.status != 200) {
+      throw data
+    }
+    return data as Character[]
+  } catch (error) {
+    console.log(error)
+  }
+  return []
+}
+
+async function fetchAllSpell(): Promise<Spell[]> {
+  try {
+    const response = await fetch(`${APIUrl}/spells`)
+    const data = await response.json()
+    if (response.status != 200) {
+      throw data
+    }
+    return data as Spell[]
+  } catch (error) {
+    console.log(error)
+  }
+  return []
+}
