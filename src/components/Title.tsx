42a52a2fcfc072daf4bdd52c530336c9b27146c2
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-16 18:23:19 +0300

    style changes of title

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -12,14 +12,16 @@ type Props = {
 
 const Title = ({ title, desc }: Props) => {
   return (
-    <div className='flex w-full items-center space-x-8'>
-      <h3
-        aria-label={title}
-        title={title}
-        className={`${harry.className} uppercase font-bold tracking-[5px] text-lg  xl:text-xl bg-clip-text bg-gradient-to-br from-red-500 from-20% via-orange-500 to-amber-500 text-transparent `}
-      >
-        {title}
-      </h3>
+    <div className='flex items-center space-x-4'>
+      <div className='bg-bg-dark px-2.5 py-2 border-b-4 border-orange-400 rounded border-r-4 border-l border-t '>
+        <h3
+          aria-label={title}
+          title={title}
+          className={`${harry.className}  uppercase font-bold tracking-[5px] text-lg  xl:text-xl bg-clip-text bg-gradient-to-br from-red-500 from-20% via-orange-500 to-amber-500 text-transparent`}
+        >
+          {title}
+        </h3>
+      </div>
       <p className='text-slate-500 text-base lg:text-lg first-letter:uppercase'>
         {desc}
       </p>
