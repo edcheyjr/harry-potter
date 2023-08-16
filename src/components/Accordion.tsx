84ba606ea0725acf34a51ca110488602f8764db6
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-16 16:09:12 +0300

    transition

--- src/components/Accordion.tsx
+++ src/components/Accordion.tsx
@@ -18,14 +18,14 @@ const Accordion = ({
   }
 
   return (
-    <div className='w-full h-auto space-y-2 '>
+    <div className='w-full h-auto space-y-2'>
       <div
-        className='transform transition duration-300 flex justify-between px-2 items-center text-slate-300 cursor-pointer py-2 hover:bg-slate-600/20 rounded'
+        className='transform transition duration-500 ease-in-out flex justify-between px-2 items-center text-slate-300 cursor-pointer py-2 hover:bg-slate-600/20 rounded'
         onClick={() => setIsOpened(!isOpened)}
       >
         {renderTitle(title)}
         <ChveronDown
-          className={`transform transition duration-300 opacity-60 h-6 w-6 fill-current  ${
+          className={`transform transition duration-500 ease-in-out opacity-60 h-6 w-6 fill-current  ${
             isOpened && 'rotate-180'
           }`}
         />
