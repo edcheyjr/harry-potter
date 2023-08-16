979e158a978f73ba1cd08c6844b640aa9034c649
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-16 12:59:57 +0300

    table styling

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 const renderTableHeadRow = (key: string, value: string) => {
   return (
-    <tr className='' key={key}>
+    <tr className='text-left' key={key}>
       <th className='capitalize bg-slate-300/5 text-slate-300'>{key}</th>
       <th className='text-ellipsis overflow-hidden text-slate-400'>{value}</th>
     </tr>
@@ -17,9 +17,9 @@ const renderTableHeadRow = (key: string, value: string) => {
 }
 const renderTableBodyRow = (key: string, value: ReactNode) => {
   return (
-    <tr className='w-full' key={key}>
-      <td className='capitalize text-slate-300'>{key}</td>
-      <td className='text-slate-400 lowercase'>{value}</td>
+    <tr className='w-full border border-zinc-400/60 border-dotted' key={key}>
+      <td className='capitalize text-slate-300 bg-slate-300/5 px-3'>{key}</td>
+      <td className='text-slate-400 lowercase px-6'>{value}</td>
     </tr>
   )
 }
@@ -27,7 +27,7 @@ const renderTableBodyRow = (key: string, value: ReactNode) => {
 export const Table = ({ head, body }: Props) => {
   return (
     <div className='w-full h-auto'>
-      <table className='table table-auto'>
+      <table className='w-full table table-auto border-spacing-y-4 border-spacing-x-1 border-2 border-zinc-400/60 border-dotted overflow-x-auto'>
         {head && (
           <thead className='w-full'>
             {Object.keys(head).map((key: string, index) =>
