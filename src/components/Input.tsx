54eb617808dc0c56a985c4cdf2d4c5feba52ff8c
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-17 11:28:43 +0300

    search input and house filter

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,14 +1,19 @@
 'use client'
 
 import React, { ChangeEvent, Dispatch, SetStateAction } from 'react'
+import SearchIcon from './icons/SearchIcon'
+import LoadingIcon from './icons/LoadingIcon'
 
 type Props = {
   input: string
   setInput: Dispatch<SetStateAction<string>>
+  isLoading: boolean
 }
 
 const renderSimpleLoadingIcon = () => {
-  return <div></div>
+  return (
+    <LoadingIcon className='animate-spin h-6 w-6 lg:h-6 lg:w-6 fill-current' />
+  )
 }
 
 const Input = (props: Props) => {
@@ -16,23 +21,35 @@ const Input = (props: Props) => {
     props.setInput(e.target.value)
   }
   return (
-    <div className='w-full'>
+    <div className='w-full text-slate-400 relative flex'>
       {/* Search Icon */}
+      <div className='absolute -translate-y-1/2 top-1/2 left-2'>
+        {props.isLoading ? (
+          renderSimpleLoadingIcon()
+        ) : (
+          <SearchIcon
+            className='h-5 w-5 lg:h-6 lg:w-6 fill-current'
+            fill='currentIcon'
+          />
+        )}
+      </div>
       <input
         type='text'
         value={props.input}
         onChange={handleChange}
-        className='w-full px-6 py-2 bg-white/5 font-medium text-white focus:bg-orange-300/10 rounded-sm'
+        className='w-full px-10 py-2.5 bg-white/[15%] font-medium focus:bg-orange-300/20 rounded-md'
         placeholder='Search a character'
       />
       {/* Eec */}
-      <span
-        title='escape'
-        aria-label='to open search press Ctrl+K'
-        className='ml-4 py-1 rounded px-2 text-center border-r border-l border-t border-b-2 bg-bg-dark border-gray-50/[25%] text-sm lg:text-base font-semibold tracking-tight'
-      >
-        Ctrl K
-      </span>
+      <div className='absolute -translate-y-1/2 top-1/2 right-2'>
+        <span
+          title='escape'
+          aria-label='to open search press Ctrl+K'
+          className='ml-4 py-1 rounded px-2 text-center border-r border-l border-t border-b-2 bg-bg-dark border-gray-50/[25%] text-sm lg:text-base font-semibold tracking-tight'
+        >
+          Ctrl K
+        </span>
+      </div>
     </div>
   )
 }
--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -5,33 +5,62 @@ import { AppContext } from '@provider/app-context'
 import React, { useContext, useState } from 'react'
 import Modal from './Modal'
 import Input from './Input'
+import { Houses } from 'types.d'
+import Image from 'next/image'
 
 type Props = {}
 
 const SearchResult = (props: Props) => {
-  const [input, setInput] = useState('')
+  const [isLoading, setLoading] = useState(false)
+  const [houseFilter, setHouseFilter] = useState<Houses | null>(null)
+  const [input, setInput] = useState('Harry Potter')
   const appContext = useContext(AppContext)
   const isOpen = appContext?.isModalOpen ?? false
   const handleCloseModal = appContext?.handleCloseModal
   const charactersArray = appContext?.characters[0]
+  const houseObj = Object.values(Houses)
+
+  console.log('houseFilter', houseFilter)
+  const handleHouseFilterSelected = (val: Houses) => {
+    setHouseFilter(val)
+  }
   return (
     <Modal isOpen={isOpen} onClose={handleCloseModal}>
       <div className='w-full'>
         {' '}
-        <Input setInput={setInput} input={input} />
+        <Input setInput={setInput} input={input} isLoading />
         <p className='text-center text-neutral-500/80 mb-4'>
           I am a modal open and close animation made with GSAP and tailwindcss.
         </p>
-        <div className='text-center'>
+        <div className='flex justify-between items-center'>
+          {/* House filter */}
+          <div className='flex w-full space-x-2'>
+            {houseObj.map((val, index) => {
+              return (
+                <div
+                  className={`p-1 flex justify-center items-center transition duration-300 ease-in-out rounded-lg border-2 border-dotted border-slate-400 hover:border-orange-300 cursor-pointer ${
+                    houseFilter == val && 'border-red-500'
+                  }`}
+                  key={val}
+                  onClick={() => handleHouseFilterSelected(val)}
+                >
+                  <Image
+                    src={require(`/public/crests/${val}.png`)}
+                    className='h-auto w-9'
+                    title={val}
+                    alt={val}
+                  />
+                </div>
+              )
+            })}
+          </div>
           <button
             onClick={appContext?.handleCloseModal}
-            className='bg-neutral-100 text-neutral-400 font-semibold text-xl rounded-md border-b-[3px] px-3 py-1'
+            className=' text-neutral-400 font-semibold text-xl rounded-md border-b-[3px] px-3 py-1'
           >
             Close
           </button>
         </div>
-        {/* House filter */}
-        <div></div>
       </div>
     </Modal>
   )
