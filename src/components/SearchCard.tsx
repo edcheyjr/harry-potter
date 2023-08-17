58228e2bbbed1122593af68b7f99789ab26d18ae
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-17 03:08:15 +0300

    started on serch card

---
+++ src/components/SearchCard.tsx
@@ -0,0 +1,10 @@
+'use client'
+import React from 'react'
+
+type Props = {}
+
+const SearchCard = (props: Props) => {
+  return <div></div>
+}
+
+export default SearchCard
--- src/components/SearchResult.tsx
+++ src/components/SearchResult.tsx
@@ -1,1 +1,35 @@
 // Result of the search and the search input itself
+import { AppContext } from '@provider/app-context'
+import React, { useContext } from 'react'
+import Modal from './Modal'
+
+type Props = {}
+
+const SearchResult = (props: Props) => {
+  const appContext = useContext(AppContext)
+  const isOpen = appContext?.isModalOpen ?? false
+  const handleCloseModal = appContext?.handleCloseModal
+  handleCloseModal
+  return (
+    <Modal isOpen={isOpen} onClose={handleCloseModal}>
+      <div>
+        {' '}
+        <h1 className='text-cyan-400 text-3xl font-bold mb-4 text-center'>
+          Hello!
+        </h1>
+        <p className='text-center text-neutral-500/80 mb-4'>
+          I am a modal open and close animation made with GSAP and tailwindcss.
+        </p>
+        <div className='text-center'>
+          <button
+            onClick={appContext?.handleCloseModal}
+            className='bg-neutral-100 text-neutral-400 font-semibold text-xl rounded-md border-b-[3px] px-3 py-1'
+          >
+            Close
+          </button>
+        </div>
+      </div>
+    </Modal>
+  )
+}
+export default SearchResult
