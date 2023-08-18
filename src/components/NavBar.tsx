d4ef1f9dec7b20f1569781fc0cdbb40e671d4f2f
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-18 10:49:37 +0300

    unecessary div

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,9 +10,7 @@ const NavBar = (props: Props) => {
       className={`z-10 border-b border-orange-500/80 fixed -top-0 left-0 right-0  transition-all ease-in-out duration-500 bg-bg-dark/30 backdrop-blur-2xl`}
     >
       <div className='flex justify-between container mx-auto max-w-7xl px-4 md:px-10 lg:px-4 2xl:px-10 py-4'>
-        <div>
-          <Logo />
-        </div>
+        <Logo />
         <SearchButton />
       </div>
     </nav>
