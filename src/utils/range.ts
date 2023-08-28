ce3503420ca9029b8cb59a074ba68bfdfb519246
Edwin Chebii <50355842+edcheyjr@users.noreply.github.com>
2023-08-28 15:51:20 +0300

    range and range unit tests

--- src/utils/range.ts
+++ src/utils/range.ts
@@ -2,7 +2,15 @@
 type T = number
 
 export const range = (start: number, end: number): Array<T> => {
+  // dont create range for negative values
+  if (start < 0 && end <= 0) {
+    return []
+  }
+  if (start < 0) {
+    start = 0
+  }
   let length = end - start + 1
+
   /*
     Create an array of certain length and set the elements within it from
     start value to end value.
