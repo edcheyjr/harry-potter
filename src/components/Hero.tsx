775a246ee11b958b7556922256a2ba61a1b66adf
edcheyjr <50355842+edcheyjr@users.noreply.github.com>
2026-02-04 09:28:07 +0300

    fix(types): update scrollToSection function to use RefObject for better type safety

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React, { MutableRefObject, useContext, useState } from 'react'
+import React, { RefObject, useContext, useState } from 'react'
 import localFont from 'next/font/local'
 import HarryPotterWorkMark from '@components/icons/HarryPotterWordmark'
 import Button from '@components/PrimaryButton'
@@ -10,48 +10,48 @@ const harry = localFont({ src: '../../public/fonts/local/HarryP.ttf' })
 type Props = {}
 
 const Hero = (props: Props) => {
-  const scrollToSection = (ref: MutableRefObject<HTMLDivElement | null>) => {
-    window.scrollTo({
-      top: ref.current?.offsetTop,
-      behavior: 'smooth',
-    })
-  }
+    const scrollToSection = (ref?: RefObject<HTMLDivElement | null>) => {
+        window.scrollTo({
+            top: ref?.current?.offsetTop,
+            behavior: 'smooth',
+        })
+    }
 
-  const appContext = useContext(AppContext)
-  const ref = appContext?.ref
-  // const [color, setColor] = useState<string>(houseColors.Gryffindor.primary)
-  return (
-    <div className='container mx-auto max-w-7xl px-8 md:px-10 flex flex-col justify-end pt-8'>
-      {/* Nav */}
-      <section className='space-y-5 flex flex-col items-center justify-center'>
-        {/* Heading Text */}
-        <h1
-          className={`${harry.className} text-5xl md:text-7xl xl:text-9xl capitalize text-white`}
-        >
-          <HarryPotterWorkMark
-            title='harry potter'
-            titleId='harry potter'
-            className='fill-current w-96 sm:w-[32rem]'
-          />
-        </h1>
+    const appContext = useContext(AppContext)
+    const ref = appContext?.ref
+    // const [color, setColor] = useState<string>(houseColors.Gryffindor.primary)
+    return (
+        <div className="container mx-auto max-w-7xl px-8 md:px-10 flex flex-col justify-end pt-8">
+            {/* Nav */}
+            <section className="space-y-5 flex flex-col items-center justify-center">
+                {/* Heading Text */}
+                <h1
+                    className={`${harry.className} text-5xl md:text-7xl xl:text-9xl capitalize text-white`}
+                >
+                    <HarryPotterWorkMark
+                        title="harry potter"
+                        titleId="harry potter"
+                        className="fill-current w-96 sm:w-[32rem]"
+                    />
+                </h1>
 
-        {/* Description */}
-        <p className='text-lg lg:text-xl xl:text-2xl font-medium text-slate-300'>
-          Characters, Know them and the world of wizardry
-        </p>
+                {/* Description */}
+                <p className="text-lg lg:text-xl xl:text-2xl font-medium text-slate-300">
+                    Characters, Know them and the world of wizardry
+                </p>
 
-        {/* Button */}
-        <div className=''>
-          <Button onClick={() => scrollToSection(ref)}>
-            <span className='w-full h-fit text-center align-middle'>
-              See Characters
-            </span>
-            <DownArrowIcon className='animate-bounce h-6 w-6 fill-current' />
-          </Button>
+                {/* Button */}
+                <div className="">
+                    <Button onClick={() => scrollToSection(ref)}>
+                        <span className="w-full h-fit text-center align-middle">
+                            See Characters
+                        </span>
+                        <DownArrowIcon className="animate-bounce h-6 w-6 fill-current" />
+                    </Button>
+                </div>
+            </section>
         </div>
-      </section>
-    </div>
-  )
+    )
 }
 
 export default Hero
